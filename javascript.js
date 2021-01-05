deca7865853ad90561034fffcc840b1ec441155f
Marissa Gonçalves <marissagoncalves13@gmail.com>
2021-01-05 10:48:13 -0500

    Initial Commit.

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function popup()
+{
+    window.alert("Hello World!!!!!");
+}	
+
+
+function pop2()
+{
+	document.write("Goodbye!!!");
+	window.alert("Goodbye!!!");
+}
+
+function changeCaption()
+{
+	document.getElementById("demo").innerHTML = "I am a Panda.";
+}
